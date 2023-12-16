08ca4e3163fccb18c8a325d04b6ec2ed9367758a
PierreKROB <krobpierre0@gmail.com>
2023-12-16 19:18:23 +0100

    la putain de ta mère quentin

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
+import styled from 'styled-components'
 import CardCharacter from '../../components/CharacterCard';
 import { useFetch } from '../../utils/hooks/userFetch';
-import { Spinner } from '@chakra-ui/react';
+import { Spinner, SimpleGrid } from '@chakra-ui/react';
+
+const CharactersContainer = styled.div`
+max-width: 1100px;
+
+`;
+
+
 
 function Home() {
-  const { data: characters, isLoading, error } = useFetch('http://localhost:3000/api/character');
+  const { data, isLoading, error } = useFetch('http://localhost:3000/api/character');
 
-  if (isLoading) {
-    return (
-      <div>
-        <h1>Bienvenue sur la page d'accueil</h1>
-        <Spinner
-          thickness='4px'
-          speed='0.65s'
-          emptyColor='gray.200'
-          color='blue.500'
-          size='xl'
-        />
-      </div>
-    );
-  }
+  const characters = data;
 
   if (error) {
     return <div>Une erreur s'est produite lors du chargement des données.</div>;
@@ -28,9 +23,29 @@ function Home() {
   return (
     <div>
       <h1>Bienvenue sur la page d'accueil</h1>
-      {characters.map(character => (
-        <CardCharacter key={character._id} character={character} />
-      ))}
+      {isLoading ? (
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+          />
+          <p>Chargement en cours...</p>
+        </div>
+      ) : (
+        <CharactersContainer>
+          <SimpleGrid max-width={1200} padding={5} spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+            {characters.map((character, index) => (
+              <CardCharacter
+                key={`${character.name}-${index}`}
+                character={character}
+              />
+            ))}
+          </SimpleGrid>
+        </CharactersContainer>
+      )}
     </div>
   );
 }
